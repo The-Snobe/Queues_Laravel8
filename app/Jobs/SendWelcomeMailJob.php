a8c8c726e4f6ff273b07c35338a7023c762c1251
THE_SNOBER <superabel000@gmail.com>
2024-03-20 06:45:38 +0100

    queue use with send message

---
+++ app/Jobs/SendWelcomeMailJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\WelcomeUserMail;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendWelcomeMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+            // Envoyer un email a user
+            Mail::to($this->user->email)->send(new WelcomeUserMail());
+    }
+}
